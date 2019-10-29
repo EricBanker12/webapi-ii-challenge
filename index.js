9e4dd5f6cbbaa1c66bcb4f7362df436f7b8a2657
ericbanker12 <ericbanker12@gmail.com>
2019-10-29 14:17:30 -0500

    create posts/posts-router

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 
+const postsRouter = require('./posts/posts-router')
+
 const server = express()
 
 const port = 5000
+
+server.use(express.json())
+server.use('/api/posts', postsRouter)
 
 server.listen(port, () => {
     console.log(`=== listening on port ${port} ===`)
