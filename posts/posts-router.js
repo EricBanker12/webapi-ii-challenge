4de7ddb7efc1246bc666c07d5cb04c656c5e7748
ericbanker12 <ericbanker12@gmail.com>
2019-10-29 14:32:41 -0500

    add POST to create new blog posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,7 +1,28 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.send('hello')
+const db = require('../data/db')
+
+// POST create a new blog post
+router.post('/', (req, res) => {
+    const {title, contents} = req.body
+    
+    if (!title || !contents || typeof title != 'string' || typeof contents != 'string') {
+        res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+    }
+    else {
+        const date = new Date()
+        const post = {title, contents, created_at: date, updated_at: date}
+
+        db.insert(post)
+        .then(resp => {
+            // console.log(resp)
+            res.json({...post, ...resp})
+        })
+        .catch(err => {
+            // console.log(err)
+            res.status(500).json({error: "There was an error while saving the post to the database."})
+        })
+    }
 })
 
 module.exports = router
